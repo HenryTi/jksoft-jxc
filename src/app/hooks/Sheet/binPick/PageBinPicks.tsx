3662c3fa89fa39fc062987ab14a47cafdb74a060
HenryTi <liaohengyi@gmail.com>
2024-02-15 22:41:36 -0500

    ok

--- src/app/hooks/Sheet/binPick/PageBinPicks.tsx
+++ src/app/hooks/Sheet/binPick/PageBinPicks.tsx
@@ -82,9 +82,10 @@ export function PageBinPicks({ sheetStore, rearPickResultType, onPicked }: Props
 
     function ViewBinPick({ binPick, onPick, index }: { binPick: BinPick; onPick: (binPick: BinPick, index: number) => Promise<void>; index: number; }) {
         let vPencil = <div className="ms-3">
-            <FA name="pencil" fixWidth={true} className="small text-info" />
+            <FA name="search" fixWidth={true} className="small text-info" />
         </div>;
         let onClick: () => Promise<void>;
+        let cnLabel: string, cnAngle: string, iconPrefix: string;
         let cn: string;
         let vContent: any;
         if (index < cur) {
@@ -95,30 +96,38 @@ export function PageBinPicks({ sheetStore, rearPickResultType, onPicked }: Props
                 {vPencil}
             </>;
             onClick = async () => await onPick(binPick, index);
-            cn = ' cursor-pointer';
+            cn = ' cursor-pointer ';
+            cnLabel = '';
+            cnAngle = ' text-success ';
+            iconPrefix = 'check-circle-o';
         }
         else if (index === cur) {
             vContent = <>
                 <div className="text-secondary flex-fill">
-                    请选择
+                    <FA name="search" className="ms-1 text-primary" size="lg" />
                 </div>
-                {vPencil}
             </>;
             onClick = async () => await onPick(binPick, index);
             cn = ' cursor-pointer';
+            cnLabel = ' text-primary fw-bold ';
+            cnAngle = ' text-primary ';
+            iconPrefix = 'angle-right';
         }
         else {
             vContent = <>
                 <small className="text-secondary">-</small>
             </>;
             cn = '';
+            cnLabel = ' text-secondary small ';
+            cnAngle = ' text-secondary small ';
+            iconPrefix = 'angle-right';
         }
         return <>
             <div className="container">
                 <div className="row">
                     <div className="col-3 py-2 tonwa-bg-gray-1">
-                        <FA name="angle-double-right" fixWidth={true} className="small me-2 text-warning" />
-                        <span className="text-secondary">{binPick.name}</span>
+                        <FA name={iconPrefix} fixWidth={true} className={'me-2 ' + cnAngle} />
+                        <span className={cnLabel}>{binPick.name}</span>
                     </div>
                     <div className={'col py-2 ' + cn} onClick={onClick}>
                         <div className="d-flex">
@@ -144,15 +153,24 @@ export function PageBinPicks({ sheetStore, rearPickResultType, onPicked }: Props
         await onPicked(ret);
     }
     return <Page header={header}>
-        <div className="p-3 tonwa-bg-gray-2">选择待处理业务</div>
+        <div className="p-3 tonwa-bg-gray-2">查询待处理业务</div>
         <Sep />
-        {binPicks.map((v, index) => {
-            const { name } = v;
-            return <ViewBinPick key={name} binPick={v} onPick={onPick} index={index} />;
-        })}
-        <ViewBinPick binPick={rearPick} onPick={onPickRear} index={binPicks.length} />
+
+        <div className="border rounded-3 mt-3">
+            <div className="tonwa-bg-gray-2 small py-3 px-3 text-secondary">
+                查询条件
+            </div>
+            <Sep />
+            {binPicks.map((v, index) => {
+                const { name } = v;
+                return <ViewBinPick key={name} binPick={v} onPick={onPick} index={index} />;
+            })}
+            <ViewBinPick binPick={rearPick} onPick={onPickRear} index={binPicks.length} />
+        </div>
         <div className="p-3">
-            <button className="btn btn-primary" onClick={onStart} disabled={rearPickResult === undefined}>开始</button>
+            <button className="btn btn-primary" onClick={onStart} disabled={rearPickResult === undefined}>
+                下一步 <FA name="arrow-right ms-2" />
+            </button>
         </div>
     </Page>;
 }
