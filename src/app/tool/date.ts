ce20d4b3909058d7c6a2ff5289db51eec5674cfa
HenryTi <liaohengyi@gmail.com>
2024-03-19 09:17:39 +0800

    ok

--- src/app/tool/date.ts
+++ src/app/tool/date.ts
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 const date19700101 = Date.parse('1970-1-1');
 const milliseconds = 1000 * 60 * 60 * 24;
 
@@ -11,7 +13,16 @@ export function fromDays(days: number) {
 export function contentFromDays(days: number) {
     if (days === undefined) return undefined;
     let date = fromDays(days);
-    let ret = date.toISOString();
-    let p = ret.indexOf('T');
-    return ret.substring(0, p);
+    //let ret = date.toISOString();
+    //let p = ret.indexOf('T');
+    //return ret.substring(0, p);
+    /*
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let ret = `${year}-${month}-${day}`;
+    return ret;
+    */
+    let ret = moment(date).format('YYYY-MM-DD');
+    return ret;
 }
