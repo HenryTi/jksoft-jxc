e6eacad1aedc1d684deccc4763795fef59ef1dd4
HenryTi <liaohengyi@gmail.com>
2024-07-30 23:07:14 -0400

    ok

--- src/app/hooks/Sheet/binEdit/divNew/inputSpec.tsx
+++ src/app/hooks/Sheet/binEdit/divNew/inputSpec.tsx
@@ -1,4 +1,4 @@
-import { BinInputSpec } from "app/Biz";
+import { BinInputSpec, BizBud, ValueSetType } from "app/Biz";
 import { Page } from "tonwa-app";
 import { theme } from "tonwa-com";
 import { useForm } from "react-hook-form";
@@ -48,14 +48,45 @@ export async function inputSpec(props: PropsInputSpec): Promise<PickResult> {
     }
     const paramValues: { [budId: number]: any } = {};
     // 暂时先按赋值处理，以后可以处理:=
+    const { id: entityId, preset, keys, buds: specBuds } = entitySpec;
+    let paramsSetCount = 0;
     for (let { bud, valueSet, valueSetType } of params) {
         paramValues[bud.id] = editing.calcValue(valueSet);
+        if (valueSetType === ValueSetType.equ) ++paramsSetCount;
     }
-
-    const viewTop = <ViewIBaseFromId sheetStore={sheetStore} valDiv={valDiv} iBase={base} />;
-    // let budsEditing: ValuesBudsEditing;
-    const { preset } = entitySpec;
-    if (preset === true) {
+    async function saveSpec(data: any) {
+        function buildValues(buds: BizBud[]) {
+            let values: { [bud: string]: number | string } = {};
+            for (let bud of buds) {
+                const { id, name, budDataType } = bud;
+                let v = data[name] ?? data[id];
+                switch (budDataType.type) {
+                    case EnumBudType.date: v = getDays(v); break;
+                }
+                values[name] = v;
+            }
+            return values;
+        }
+        const keyValues = buildValues(keys);
+        const propValues = buildValues(specBuds);
+        const param: ParamSaveSpec = {
+            id: undefined,
+            spec: entityId,
+            base,
+            keys: keyValues,
+            props: propValues,
+        };
+        let results = await uq.SaveSpec.submit(param);
+        let { id } = results;
+        if (id < 0) id = -id;
+        let retSpec = Object.assign(data, { id, base });
+        return retSpec;
+    }
+    let ret: any;
+    if (paramsSetCount === keys.length + specBuds.length) {
+        ret = await saveSpec(paramValues);
+    }
+    else if (preset === true) {
         let { ret } = await uq.GetSpecsFromBase.query({ base });
         let retSpec: any;
         switch (ret.length) {
@@ -64,14 +95,72 @@ export async function inputSpec(props: PropsInputSpec): Promise<PickResult> {
             case 0: retSpec = { id: base }; break;
             case 1: retSpec = ret[0]; break;
         }
-        return retSpec;
+        ret = retSpec;
     }
-    const { keys, buds: specBuds } = entitySpec;
-    let buds = [...keys];
-    if (specBuds !== undefined) buds.push(...specBuds);
-    let budsEditing = new ValuesBudsEditing(modal, biz, buds);
-    budsEditing.initBudValues(paramValues);
-    let ret = await modal.open(<PagePickSpec />);
+    else {
+        let buds = [...keys];
+        if (specBuds !== undefined) buds.push(...specBuds);
+        let budsEditing = new ValuesBudsEditing(modal, biz, buds);
+        budsEditing.initBudValues(paramValues);
+        ret = await modal.open(<PagePickSpec />);
+        function PagePickSpec() {
+            const { caption, name } = entitySpec;
+            const { register, handleSubmit, formState: { errors } } = useForm({ mode: 'onBlur' });
+            const submitCaption = btnNext;
+            const submitClassName: string = cnNextClassName;
+
+            let formRows: FormRow[] = [
+                ...budsEditing.buildFormRows(),
+                { type: 'submit', label: submitCaption, options: {}, className: submitClassName }
+            ];
+            const onSubmitForm = async (data: any) => {
+                /*
+                const keyValues: { [bud: string]: number | string } = {};
+                for (let key of keys) {
+                    const { name, budDataType } = key;
+                    let v = data[name];
+                    switch (budDataType.type) {
+                        case EnumBudType.date: v = getDays(v); break;
+                    }
+                    keyValues[name] = v;
+                }
+                const propValues: { [bud: string]: number | string } = {};
+                for (let prop of props) {
+                    const { name } = prop;
+                    propValues[name] = data[name];
+                }
+                const param: ParamSaveSpec = {
+                    id: undefined,
+                    spec: entityId,
+                    base,
+                    keys: keyValues,
+                    props: propValues,
+                };
+                let results = await uq.SaveSpec.submit(param);
+                let { id } = results;
+                if (id < 0) id = -id;
+                let retSpec = Object.assign(data, { id, base });
+                */
+                let retSpec = await saveSpec(data);
+                modal.close(retSpec);
+            }
+            const { bootstrapContainer } = theme;
+            return <Page header={caption ?? name}>
+                <div className={bootstrapContainer + ' pt-3 tonwa-bg-gray-2 '}>
+                    <Band>
+                        <div className="mx-3">
+                            <ViewIBaseFromId sheetStore={sheetStore} valDiv={valDiv} iBase={base} />
+                        </div>
+                    </Band>
+                </div>
+                <div className="m-3">
+                    <form className={bootstrapContainer} onSubmit={handleSubmit(onSubmitForm)}>
+                        <FormRowsView rows={formRows} register={register} errors={errors} context={budsEditing} />
+                    </form>
+                </div>
+            </Page>;
+        }
+    }
     if (ret !== undefined) {
         const { id, base } = ret;
         const { bizSpecColl, bizAtomColl } = sheetStore;
@@ -81,58 +170,6 @@ export async function inputSpec(props: PropsInputSpec): Promise<PickResult> {
         }
     }
     return ret;
-    function PagePickSpec() {
-        const { id: entityId, caption, name, keys, buds: props } = entitySpec;
-        const { register, handleSubmit, formState: { errors } } = useForm({ mode: 'onBlur' });
-        const submitCaption = btnNext;
-        const submitClassName: string = cnNextClassName;
-
-        let formRows: FormRow[] = [
-            ...budsEditing.buildFormRows(),
-            { type: 'submit', label: submitCaption, options: {}, className: submitClassName }
-        ];
-        const onSubmitForm = async (data: any) => {
-            const keyValues: { [bud: string]: number | string } = {};
-            for (let key of keys) {
-                const { name, budDataType } = key;
-                let v = data[name];
-                switch (budDataType.type) {
-                    case EnumBudType.date: v = getDays(v); break;
-                }
-                keyValues[name] = v;
-            }
-            const propValues: { [bud: string]: number | string } = {};
-            for (let prop of props) {
-                const { name } = prop;
-                propValues[name] = data[name];
-            }
-            const param: ParamSaveSpec = {
-                id: undefined,
-                spec: entityId,
-                base,
-                keys: keyValues,
-                props: propValues,
-            };
-            let results = await uq.SaveSpec.submit(param);
-            let { id } = results;
-            if (id < 0) id = -id;
-            let retSpec = Object.assign(data, { id, base });
-            modal.close(retSpec);
-        }
-        const { bootstrapContainer } = theme;
-        return <Page header={caption ?? name}>
-            <div className={bootstrapContainer + ' pt-3 tonwa-bg-gray-2 '}>
-                <Band>
-                    <div className="mx-3">{viewTop}</div>
-                </Band>
-            </div>
-            <div className="m-3">
-                <form className={bootstrapContainer} onSubmit={handleSubmit(onSubmitForm)}>
-                    <FormRowsView rows={formRows} register={register} errors={errors} context={budsEditing} />
-                </form>
-            </div>
-        </Page>;
-    }
 
     function PagePickSelect() {
         function onClick() {
