9437f253283f2d1f08504889d240c393c3156655
HenryTi <liaohengyi@gmail.com>
2025-01-25 17:32:07 -0500

    ok

--- src/app/hooks/Query/fromQuery.tsx
+++ src/app/hooks/Query/fromQuery.tsx
@@ -1,12 +1,12 @@
-import { EntityQuery, IDColumn, PickQuery } from "app/Biz";
+import { IDColumn, PickQuery } from "app/Biz";
 import { ChangeEvent, useState } from "react";
 import { Page } from "tonwa-app";
-import { LabelRow, List, Sep, theme } from "tonwa-com";
+import { List, Sep, theme } from "tonwa-com";
 import { RearPickResultType } from "../Sheet/store";
-import { LabelBox, Prop, QueryRow, QueryRowCol, RowCols } from "app/hooks/tool";
+import { QueryRow, QueryRowCol, RowCols } from "app/hooks/tool";
 import { QueryStore } from "app/hooks/Query";
 import { BizPhraseType } from "uqs/UqDefault";
-import { ViewAtomPrimesOfStore, ViewAtomTitlesOfStore, ViewForkAtom, ViewForkAtomBold } from "../View";
+import { ViewAtomPrimesOfStore, ViewAtomTitlesOfStore, ViewForkAtomBold, ViewForkBuds } from "../View";
 import { ViewForkId } from "app/coms/ViewForkId";
 import { ViewBud } from "../Bud";
 import { BudsEditing } from "../BudsEditing";
@@ -18,26 +18,11 @@ async function pickFromQueryBase(
     , binPick: PickQuery
     , pickResultType: RearPickResultType)
     : Promise<PickResult | PickResult[]> {
-    let { caption, query, pickParams } = binPick as PickQuery;
+    let { query } = binPick as PickQuery;
     const { modal } = editing;
     let queryStore = new QueryStore(modal, query);
     let ret = await modal.open(<PageFromQuery />);
     if (ret === undefined) return;
-    /*
-    function toRet(v: any) {
-        let obj = {} as any;
-        for (let i in v) {
-            let p = v[i];
-            if (typeof p === 'object') {
-                obj[i] = p.value;
-            }
-            else {
-                obj[i] = p;
-            }
-        }
-        return obj;
-    }
-    */
     function toRet(queryRow: QueryRow) {
         let { ids, cols } = queryRow;
         let ret: any = { id: ids[ids.length - 1] };
@@ -52,16 +37,12 @@ async function pickFromQueryBase(
         return toRet(ret);
     }
     else {
-        // return (ret as any[]).map(v => toRet(v));
         return (ret as QueryRow[]).map(v => toRet(v));
     }
-    // return ret;
 
     function PageFromQuery() {
-        //let isPick = false;
-        const { caption, name, ids: idCols, showIds: showIdCols, idFrom, value: budValue, hideCols, subCols } = query;
+        const { caption, name, ids: idCols, idFrom, value: budValue, hideCols } = query;
         const header = caption ?? name;
-        const { bizPhraseType } = idFrom;
         let [list, setList] = useState(null as QueryRow[]);
         let [selectedItems, setSelectedItems] = useState<{ [id: number]: QueryRow; }>({});
         async function onSearch(params: any) {
@@ -69,7 +50,6 @@ async function pickFromQueryBase(
             setList(queryResults);
         }
         function closeModal(results: any) {
-            // Object.assign(editing.store.bizAtomColl, queryStore.bizAtomColl);
             editing.store.mergeStoreColl(queryStore);
             modal.close(results);
         }
@@ -107,10 +87,9 @@ async function pickFromQueryBase(
         let cnViewItem = 'd-flex flex-wrap ';
         let cnItem: string;
         let btnOk: any;
-        //if (isPick === true) {
         if (pickResultType === RearPickResultType.array) {
             onItemSelect = onMultipleClick;
-            cnItem = ' my-2 ';
+            cnItem = ' px-3 my-2 ';
             btnOk = <button className="btn btn-primary m-3" onClick={onPick}>选入</button>;
         }
         else {
@@ -118,88 +97,53 @@ async function pickFromQueryBase(
             cnViewItem += 'ps-3 ';
             cnItem = 'py-2 px-3 border-bottom ';
         }
-        //}
         function ViewPropArr({ propArr }: { propArr: QueryRowCol[]; }) {
             if (propArr === undefined) return null;
             return <>
                 {propArr.map((v, index) => {
                     const [bud, value] = v;
                     if (hideCols[bud.id] === true) return null;
-                    /*
-                    if (subCols !== undefined) {
-                        if (subCols[id] !== true) return null;
-                    }
-                    */
                     return <ViewBud key={index} bud={bud} value={value} store={queryStore} />;
                 })}
             </>;
         }
-        function ViewShowIds({ ids }: { ids: number[]; }) {
-            if (ids === undefined) return null;
-            return <>{showIdCols.map((v, index) => {
-                const { ui, alias } = v;
-                let caption: string;
-                if (ui === undefined) {
-                    caption = alias;
-                }
-                else {
-                    caption = ui.caption ?? alias;
-                }
-                function ViewId({ id, col }: { id: number; col: IDColumn; }) {
-                    let colFromEntity = query.getFromEntityFromAlias(col.alias);
-                    const { bizPhraseType } = colFromEntity;
-                    switch (bizPhraseType) {
-                        default:
-                            return <>unknown bizPhraseType {bizPhraseType}</>
-                        case BizPhraseType.fork:
-                            return <ViewForkId id={id} />;
-                        case BizPhraseType.atom:
-                            return <ViewForkAtom id={id} store={queryStore} />;
-                    }
-                }
-                return <LabelBox key={index} label={caption}>
-                    <ViewId id={ids[index]} col={v} />
-                </LabelBox>;
-            })}</>;
+        function ViewIdOne({ id, col, cols }: { id: number; col: IDColumn; cols: QueryRowCol[]; }) {
+            let colFromEntity = query.getFromEntityFromAlias(col.alias);
+            const { bizPhraseType } = colFromEntity;
+            switch (bizPhraseType) {
+                default:
+                    return <>unknown bizPhraseType {bizPhraseType}</>
+                case BizPhraseType.fork:
+                    if (id === undefined) return null;
+                    return <ViewForkBuds id={id} store={queryStore} />;
+                case BizPhraseType.atom:
+                    return <div>
+                        <div>
+                            <ViewForkAtomBold id={id} store={queryStore} />
+                            <ViewAtomTitlesOfStore id={id} store={queryStore} />
+                        </div>
+                        <RowCols contentClassName="">
+                            <ViewAtomPrimesOfStore id={id} store={queryStore} />
+                            <ViewPropArr propArr={cols} />
+                        </RowCols>
+                    </div>;
+            }
         }
-        function ViewItemAtomContent({ value: picked }: { value: QueryRow }) {
+        function ViewItemMain({ value: picked }: { value: QueryRow }) {
             const { ids, cols } = picked;
-            if (ids === undefined) return null;
-            // let propArr: Prop[] = picked.$ as any;
-            function ViewIdOne({ id, col }: { id: number; col: IDColumn; }) {
-                let colFromEntity = query.getFromEntityFromAlias(col.alias);
-                const { bizPhraseType } = colFromEntity;
-                switch (bizPhraseType) {
-                    default:
-                        return <>unknown bizPhraseType {bizPhraseType}</>
-                    case BizPhraseType.fork:
-                        if (id === undefined) return null;
-                        return <ViewForkId id={id} />;
-                    case BizPhraseType.atom:
-                        return <div>
-                            <div>
-                                <ViewForkAtomBold id={id} store={queryStore} />
-                                <ViewAtomTitlesOfStore id={id} store={queryStore} />
-                            </div>
-                            <RowCols contentClassName="">
-                                <ViewAtomPrimesOfStore id={id} store={queryStore} />
-                                <ViewPropArr propArr={cols} />
-                            </RowCols>
-                        </div>;
-                }
-            }
-            function onCheckChange(evt: ChangeEvent<HTMLInputElement>) {
-                const { checked } = evt.currentTarget;
-                onMultipleClick(picked, checked);
-            }
             return <>
                 {idCols.map((v, index) => {
                     let col = v;
                     let val = ids[index];
                     if (index >= idCols.length) return null;
-                    return <ViewIdOne key={index} id={val} col={col} />
+                    return <ViewIdOne key={index} id={val} col={col} cols={cols} />
                 })}
             </>;
+        }
+        function ViewItemDetail({ value: picked }: { value: QueryRow }) {
+            const { ids, cols } = picked;
+            let indexLast = idCols.length - 1;
+            return <ViewIdOne id={ids[0]} col={idCols[indexLast]} cols={cols} />
         }
         function ViewValue({ value, caption }: { value: number; caption: string; }) {
             if (budValue === undefined) return null;
@@ -215,7 +159,7 @@ async function pickFromQueryBase(
             if (rowId > 0) {
                 vHead = <div className={cnItem + ' d-flex '}>
                     <div className="flex-fill">
-                        <ViewItemAtomContent value={picked} />
+                        <ViewItemMain value={picked} />
                     </div>
                     <ViewValue value={picked.values?.[0]?.[1]} caption="数量" />
                 </div>;
@@ -232,10 +176,10 @@ async function pickFromQueryBase(
                             defaultChecked={selectedItems[value.rowId] !== undefined}
                             onChange={onCheckChange}
                         />
-                        <ViewItemAtomContent value={value} />
+                        <ViewItemDetail value={value} />
                     </label>;
                 }
-                return <div className={cn}><ViewItemAtomContent value={value} /></div>;
+                return <div className={cn}><ViewItemDetail value={value} /></div>;
             }
             // onItemSelect={onItemSelect}
             return <div>
